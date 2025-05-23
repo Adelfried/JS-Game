a4a4af38958a130b04f622a28995f273d6c46c48
KevinNishimura123 <kevinblack9j@gmail.com>
2025-05-23 17:57:37 -0400

    agregando orbes chistosas

--- Script.js
+++ Script.js
@@ -1,10 +1,57 @@
-console.log("script.js loaded");
-
-// Para que el canvas ocupe toda la pantalla
 var canvas = document.querySelector("canvas");
-console.log(canvas);
-var ctx = canvas.getContext("2d");
+var c = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = "blue";
+        c.stroke();
+        c.fillStyle = "blue";
+        c.fill();
+    };
+
+    this.update = function() {
+        // Bounce off left/right
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        // Bounce off top/bottom
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    };
+}
+
+var circleArray = [];
+for (var i = 0; i < 100; i++) {
+    var radius = 30;
+    var x = Math.random() * (canvas.width - 2 * radius) + radius;
+    var y = Math.random() * (canvas.height - 2 * radius) + radius;
+    var dx = (Math.random() - 0.5) * 10;
+    var dy = (Math.random() - 0.5) * 10;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+}
+animate();
 
