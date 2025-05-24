d50e80b64add67ad3f7f45829c59c452c01af928
KevinNishimura123 <kevinblack9j@gmail.com>
2025-05-24 13:18:26 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,57 +1,98 @@
 var canvas = document.querySelector("canvas");
 var c = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 64*16;
+canvas.height = 64*9;
 
-function Circle(x, y, dx, dy, radius) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
-
-    this.draw = function() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = "blue";
-        c.stroke();
-        c.fillStyle = "blue";
-        c.fill();
-    };
-
-    this.update = function() {
-        // Bounce off left/right
-        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
-            this.dx = -this.dx;
+class Player {
+    constructor() {
+    this.position = {
+        x: 100,
+        y: 100
         }
-        // Bounce off top/bottom
-        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
-            this.dy = -this.dy;
+        this.velocity = {
+            x: 0,
+            y: 0,
         }
-        this.x += this.dx;
-        this.y += this.dy;
 
-        this.draw();
-    };
-}
-
-var circleArray = [];
-for (var i = 0; i < 100; i++) {
-    var radius = 30;
-    var x = Math.random() * (canvas.width - 2 * radius) + radius;
-    var y = Math.random() * (canvas.height - 2 * radius) + radius;
-    var dx = (Math.random() - 0.5) * 10;
-    var dy = (Math.random() - 0.5) * 10;
-    circleArray.push(new Circle(x, y, dx, dy, radius));
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < circleArray.length; i++) {
-        circleArray[i].update();
+        this.width = 100
+        this.height = 100
+        this.sides = {
+            bottom: this.position.y + this.height
+        }
+        this.gravity = 1
+    }
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update (){
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        this.sides.bottom = this.position.y + this.height
+        //above bottom of canvas
+        if (this.sides.bottom + this.velocity.y < canvas.height){
+            this.velocity.y += 0.7
+            
+        }
+        else this.velocity.y = 0
     }
 }
-animate();
 
+const player = new Player ()
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+function animate(){
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect (0, 0, canvas.width, canvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+
+    player.velocity.x = 0
+    if (keys.d.pressed) {player.velocity.x = 5} 
+    else if (keys.a.pressed) {player.velocity.x = -5}
+
+    player.draw()
+    player.update()
+}
+
+animate ()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'w':
+          if (player.velocity.y === 0)
+            player.velocity.y = -20
+
+        break
+        case 'a':
+        keys.a.pressed = true
+        //izquierda
+        break
+        case 'd':
+        keys.d.pressed = true
+    }
+
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'a':
+        keys.a.pressed = false
+        //izquierda
+        break
+        case 'd':
+        keys.d.pressed = false
+    }
+
+})
